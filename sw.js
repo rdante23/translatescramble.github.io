092c5e747fb9d02af467e66e1f46652393b0a2a6
rdante23 <63917367+rdante23@users.noreply.github.com>
2025-07-24 08:20:25 +0700

    Add files via upload
    Version 1 of Tranlate and Unscramble

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'translate-game-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
